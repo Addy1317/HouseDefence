f26febdd1b1546f3a78bcc47fb8bfa3de0633e32
Aditya Singh <s.addy1317@gmail.com>
2024-12-14 19:48:56 +0530

    feat:PauseMenu
    - Settings Panel
    - Pause Menu Buttons

---
+++ Assets/Scripts/ManagersScripts/GameService.cs
@@ -0,0 +1,12 @@
+using HouseDefence.Generic;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HouseDefence.Services
+{
+    public class GameService : GenericMonoSingleton<GameService>
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UIScripts/PauseMenuUI.cs
@@ -0,0 +1,86 @@
+#region Summary
+#endregion
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace HouseDefence.UI
+{
+    public class PauseMenuUI : MonoBehaviour
+    {
+        [Header("PaueMenu Buttons")]
+        [SerializeField] private GameObject _pauseMenuPanel;
+        [SerializeField] private GameObject _settingsPanel;
+        [SerializeField] private Button _resumeButton;
+        [SerializeField] private Button _restartButton;
+        [SerializeField] private Button _homeButton;
+        [SerializeField] private Button _settingsButton;
+        [SerializeField] private Button _quitButton;
+
+        [Header("Settings Panel Buttons")]
+        [SerializeField] private Button _settingsPanelCloseButton;
+
+        private void OnEnable()
+        {
+            _resumeButton.onClick.AddListener(OnResumeButton);
+            _restartButton.onClick.AddListener(OnRestartButton);
+            _homeButton.onClick.AddListener(OnHomeButton);
+            _settingsButton.onClick.AddListener(OnSettingsButton);
+            _quitButton.onClick.AddListener(OnQuitButton);
+
+            _settingsPanelCloseButton.onClick.AddListener(OnSettingsPanelCloseButton);
+        }
+
+        private void OnDisable()
+        {
+            _resumeButton.onClick.RemoveListener(OnResumeButton);
+            _restartButton.onClick.RemoveListener(OnRestartButton);
+            _homeButton.onClick.RemoveListener(OnHomeButton);
+            _settingsButton.onClick.RemoveListener(OnSettingsButton);
+            _quitButton.onClick.RemoveListener(OnQuitButton);
+
+            _settingsPanelCloseButton.onClick.RemoveListener(OnSettingsPanelCloseButton);
+        }
+
+        #region PauseMenu Buttons Methods
+
+        private void OnResumeButton()
+        {
+            _pauseMenuPanel.SetActive(false);
+        }
+
+        private void OnRestartButton()
+        {
+            SceneManager.LoadScene("MainGame");
+        }
+
+        private void OnHomeButton()
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+
+        private void OnSettingsButton()
+        {
+            _settingsPanel.SetActive(true);
+        }
+
+        private void OnQuitButton()
+        {
+            Application.Quit();
+        }
+        #endregion
+
+        #region SettingsPanel Buttons Methods
+        private void OnSettingsPanelCloseButton()
+        {
+            _settingsPanel.SetActive(false);
+        }
+        #endregion
+
+        public void OnPauseButton()
+        {
+            //creditsPanel.SetActive(!creditsPanel.activeSelf);
+            _pauseMenuPanel.SetActive(!_pauseMenuPanel.activeSelf);
+        }
+    }
+}
