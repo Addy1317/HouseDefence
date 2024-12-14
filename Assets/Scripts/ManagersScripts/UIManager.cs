305a98caf75d0b3d5c45f7832c3212a5491e4b8f
Aditya Singh <s.addy1317@gmail.com>
2024-12-14 20:48:40 +0530

    feat: UIManager
    - UI Manager

---
+++ Assets/Scripts/ManagersScripts/UIManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HouseDefence.UI
+{
+    public class UIManager : MonoBehaviour
+    {
+        [Header("Pause Menu UI")]
+        [SerializeField] private GameObject _pauseMenuUI;
+
+        private void Update()
+        {
+            InputsforPauseButton();   
+        }
+
+        private void InputsforPauseButton()
+        {
+           if( Input.GetKeyDown(KeyCode.P))
+           {
+                _pauseMenuUI.SetActive(!_pauseMenuUI.activeSelf);
+           }
+        }
+    }
+}
